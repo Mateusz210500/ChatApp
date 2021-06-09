ffd2367632c2adbc90912717f352f959dc4f22fb
Mateusz210500 <mateusz200005@gmail.com>
2021-06-09 12:28:07 +0200

    added singing up

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,25 +1,47 @@
 import { FormField } from 'components'
-import { Formik, Form, ErrorMessage, Field} from 'formik'
+import {useHistory} from 'react-router-dom'
+import { Formik, Form } from 'formik'
 import { defaultValues, validationSchema } from './formikConfig'
+import {useState} from 'react'
 
 
 export const Signup = () => {
+
+    const history = useHistory();
+    const [serverError, setServerError] = useState('');
+
+    const signup = ({email, userName, password}, {setSubmitting}) => 
+    console.log('Signing Up: ', email, userName, password);
 
     return(
         <div className="auth-form">
             <h1>Signup</h1>
             <Formik 
-            onSubmit ={() => console.log('Submittin')}
+            onSubmit ={signup}
             validateOnMount={true}
             initialValues={defaultValues}
             validationSchema={validationSchema}
             >
                 {({isValid, isSubmitting}) => (
                     <Form>
-                        <FormField name="userName" label="Username"/>
+                        <FormField name="userName" label="User Name"/>
+                        <FormField name="email" label="Email" type="email"/>
+                        <FormField name="password" label="Password" type="password"/>
+                        <FormField name="verifyPassword" label="Verify Password" type="password" />
+
+                        <div className="auth-link-container">
+                            Already have an account?{' '}
+                            <span className="auth-link" onClick={() => history.push('login')}>
+                                Log In!
+                            </span>
+                        </div>
+
+                        <button disabled={isSubmitting || !isValid} type="submit">Sign Up</button>
                     </Form>
                 )}
             </Formik>
+
+            {!!serverError && <div className='error'>{serverError}</div>}
         </div>
     )
 
